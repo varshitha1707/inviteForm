a74e1fd4599db0f314d7af3b2a78dafea3b9f046
varshitha1707 <varshithasreddy@gmail.com>
2024-12-19 17:41:36 +0530

    updated server w firebase

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('./firebasecreds.json') // Replace with your Firebase Admin SDK key
-
+const serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key.replace(/\\n/g, '\n'), // Replace escaped newlines for private key
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+}
 initializeApp({
   credential: cert(serviceAccount),
 })
